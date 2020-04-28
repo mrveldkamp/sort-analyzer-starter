a9382063065d096391e218f5146988340ad29975
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2020-04-28 12:54:07 -0600

    modified solution code

---
+++ js/search-sort.js
@@ -0,0 +1,16 @@
+// Library of Search & Sort Functions
+
+// Use Bubble Sort algorithm to sort array argument
+function bubbleSort(anArray) {
+
+}
+
+// Use Selection Sort algorithm to sort array argument
+function selectionSort(anArray) {
+
+}
+
+// Use Insertion Sort algorithm to sort array argument
+function insertionSort(anArray) {
+
+}
